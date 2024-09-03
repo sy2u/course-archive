91f14b8967a0861d4f4b16853a73451c03a6a40c
siyingy3 <siyingy3@fastx3-29.ews.illinois.edu>
2024-09-03 01:51:31 -0500

    finish part1-lfsr

--- mp_verif/lfsr/hdl/lfsr.sv
+++ mp_verif/lfsr/hdl/lfsr.sv
@@ -10,4 +10,14 @@ module lfsr #(
 
     // TODO: Fill this out!
 
+    always_ff @(posedge clk) begin
+        if(rst) begin
+            shift_reg <= SEED_VALUE;
+        end
+        if(en) begin
+            rand_bit <= shift_reg[0];
+            shift_reg <= {shift_reg[2]^shift_reg[3]^shift_reg[5]^shift_reg[0], shift_reg[15:1]};
+        end
+    end
+
 endmodule : lfsr
