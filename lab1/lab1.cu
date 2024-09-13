987d41a068626d1a72d2e7588c183e3fa388bc13
Siying Yu <syu4@dt-login01.delta.ncsa.illinois.edu>
2024-09-13 02:11:14 -0500

    finish  lab1

--- lab1/lab1.cu
+++ lab1/lab1.cu
@@ -3,6 +3,8 @@
 
 __global__ void vecAdd(float *in1, float *in2, float *out, int len) {
   //@@ Insert code to implement vector addition here
+  int i = blockIdx.x * blockDim.x + threadIdx.x;
+  if( i < len ){ out[i] = in1[i] + in2[i]; }
 }
 
 int main(int argc, char **argv) {
@@ -11,6 +13,9 @@ int main(int argc, char **argv) {
   float *hostInput1;
   float *hostInput2;
   float *hostOutput;
+  float *deviceInput1;
+  float *deviceInput2;
+  float *deviceOutput;
 
   args = wbArg_read(argc, argv);
   //@@ Importing data and creating memory on host
@@ -23,21 +28,30 @@ int main(int argc, char **argv) {
   wbLog(TRACE, "The input length is ", inputLength);
 
   //@@ Allocate GPU memory here
-
+  cudaMalloc((void**)&deviceInput1, inputLength*sizeof(float) );
+  cudaMalloc((void**)&deviceInput2, inputLength*sizeof(float) );
+  cudaMalloc((void**)&deviceOutput, inputLength*sizeof(float) );
 
   //@@ Copy memory to the GPU here
-
+  cudaMemcpy(deviceInput1, hostInput1, inputLength*sizeof(float), cudaMemcpyHostToDevice);
+  cudaMemcpy(deviceInput2, hostInput2, inputLength*sizeof(float), cudaMemcpyHostToDevice);
 
   //@@ Initialize the grid and block dimensions here
-
+  dim3 DimGrid(inputLength/256, 1, 1);
+  if(0 != inputLength % 256){ DimGrid.x++; }
+  dim3 DimBlock(256, 1, 1);
 
   //@@ Launch the GPU Kernel here to perform CUDA computation
+  vecAdd<<<DimGrid,DimBlock>>>(deviceInput1, deviceInput2, deviceOutput, inputLength);
 
   cudaDeviceSynchronize();
   //@@ Copy the GPU memory back to the CPU here
-
+  cudaMemcpy(hostOutput, deviceOutput, inputLength*sizeof(float), cudaMemcpyDeviceToHost);
 
   //@@ Free the GPU memory here
+  cudaFree(deviceInput1);
+  cudaFree(deviceInput2);
+  cudaFree(deviceOutput);
 
 
   wbSolution(args, hostOutput, inputLength);
