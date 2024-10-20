cbb10c1f162338aa4cffb4ea10221f2a8a2bd60d
Siying Yu <syu4@dt-login03.delta.ncsa.illinois.edu>
2024-10-20 08:10:33 -0500

    finish lab5

--- lab5/lab5.cu
+++ lab5/lab5.cu
@@ -18,8 +18,26 @@
   
 __global__ void total(float *input, float *output, int len) {
   //@@ Load a segment of the input vector into shared memory
+  __shared__ float partialSum[2*BLOCK_SIZE];
+  unsigned int t = threadIdx.x;
+  unsigned int idx1 = t + 2*blockDim.x*blockIdx.x;
+  unsigned int idx2 = idx1 + blockDim.x;
+  partialSum[t] = input[idx1];
+  if( idx2 < len ){
+    partialSum[blockDim.x+t] = input[idx2];
+  } else {
+    partialSum[blockDim.x+t] = 0;
+  }
+  __syncthreads();
   //@@ Traverse the reduction tree
+  for( unsigned int stride = blockDim.x; stride >= 1; stride >>= 1 ){
+    __syncthreads();
+    if( t < stride ){
+      partialSum[t] += partialSum[stride+t];
+    }
+  }
   //@@ Write the computed sum of the block to the output vector at the correct index
+  if( t == 0 ){ output[blockIdx.x] = partialSum[0]; }
 }
 
 int main(int argc, char **argv) {
@@ -28,6 +46,8 @@ int main(int argc, char **argv) {
   float *hostInput;  // The input 1D list
   float *hostOutput; // The output list
   //@@ Initialize device input and output pointers
+  float *deviceInput;
+  float *deviceOutput;
 
   int numInputElements;  // number of elements in the input list
   int numOutputElements; // number of elements in the output list
@@ -48,20 +68,22 @@ int main(int argc, char **argv) {
   // The number of output elements in the input is numOutputElements
 
   //@@ Allocate GPU memory
-
+  cudaMalloc((void**)&deviceInput, sizeof(float)*numInputElements);
+  cudaMalloc((void**)&deviceOutput, sizeof(float)*numOutputElements);
 
   //@@ Copy input memory to the GPU
-
+  cudaMemcpy(deviceInput, hostInput, sizeof(float)*numInputElements, cudaMemcpyHostToDevice);
 
   //@@ Initialize the grid and block dimensions here
-
+  dim3 BlockDim (BLOCK_SIZE,1,1);
+  dim3 GridDim (numOutputElements,1,1);
 
   //@@ Launch the GPU Kernel and perform CUDA computation
-
+  total<<<GridDim,BlockDim>>>(deviceInput,deviceOutput,numInputElements);
   
   cudaDeviceSynchronize();  
   //@@ Copy the GPU output memory back to the CPU
-
+  cudaMemcpy(hostOutput,deviceOutput,sizeof(float)*numOutputElements,cudaMemcpyDeviceToHost);
   
   /********************************************************************
    * Reduce output vector on the host
@@ -73,8 +95,12 @@ int main(int argc, char **argv) {
     hostOutput[0] += hostOutput[ii];
   }
 
-  //@@ Free the GPU memory
+  // debug
+  // wbLog(TRACE, hostOutput[0]);
 
+  //@@ Free the GPU memory
+  cudaFree(deviceInput);
+  cudaFree(deviceOutput);
 
 
   wbSolution(args, hostOutput, 1);
