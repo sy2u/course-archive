af2e60cd7766279d92d316b45182f7d1fbbe8901
Siying Yu <syu4@dt-login02.delta.ncsa.illinois.edu>
2024-10-27 16:29:18 -0500

    finish lab6

--- lab6/lab6.cu
+++ lab6/lab6.cu
@@ -6,7 +6,7 @@
 
 #include <wb.h>
 
-#define BLOCK_SIZE 512 //@@ You can change this
+#define BLOCK_SIZE 1024 //@@ You can change this
 
 #define wbCheck(stmt)                                                     \
   do {                                                                    \
@@ -18,11 +18,57 @@
     }                                                                     \
   } while (0)
 
-__global__ void scan(float *input, float *output, int len) {
+__global__ void scan_tree(float *input, float *output, int len) {
   //@@ Modify the body of this function to complete the functionality of
   //@@ the scan on the device
   //@@ You may need multiple kernel calls; write your kernels before this
   //@@ function and call them from the host
+  __shared__ float T[2*BLOCK_SIZE];
+  // each thread load 2 inputs
+  int i = 2*(blockIdx.x*blockDim.x+threadIdx.x);
+  int j = i+1;
+  if( i < len ) T[2*threadIdx.x] = input[i];
+  if( j < len ) T[2*threadIdx.x+1] = input[j];
+  // first pass
+  int stride = 1;
+  while( stride < 2*BLOCK_SIZE ){
+    __syncthreads();
+    int idx = (threadIdx.x+1)*stride*2 - 1;
+    if(idx < 2*BLOCK_SIZE && (idx-stride) >= 0)
+    T[idx] += T[idx-stride];
+    stride = stride*2;
+  }
+  // second pass: post scan
+  stride = BLOCK_SIZE/2;
+  while(stride > 0) {
+    __syncthreads();
+    int idx = (threadIdx.x+1)*stride*2 - 1;
+    if ((idx+stride) < 2*BLOCK_SIZE)
+    T[idx+stride] += T[idx];
+    stride = stride / 2;
+  }
+  // copy back
+  __syncthreads();
+  if( i < len ) output[i] = T[2*threadIdx.x];
+  if( j < len ) output[j] = T[2*threadIdx.x+1];
+}
+
+__global__ void scan_getsum(float *input, float *sums, int numblocks) {
+  // 511, 1023, ..., must be odd
+  if( (threadIdx.x == BLOCK_SIZE-1) && (blockIdx.x < numblocks) ){
+    sums[blockIdx.x] = input[2*(threadIdx.x+blockIdx.x*blockDim.x)+1];
+  }
+}
+
+__global__ void scan_add(float *input, float *sums, int len) {
+  int sum;
+  if( blockIdx.x!=0 ){
+    sum = sums[blockIdx.x-1];
+    int i = 2*(blockIdx.x*blockDim.x+threadIdx.x);
+    int j = i+1;
+    if( i < len ) input[i] = input[i] + sum;
+    if( j < len ) input[j] = input[j] + sum;
+  }
 }
 
 int main(int argc, char **argv) {
@@ -54,10 +100,18 @@ int main(int argc, char **argv) {
                      cudaMemcpyHostToDevice));
 
   //@@ Initialize the grid and block dimensions here
-
+  int numblocks = (numElements-1)/(BLOCK_SIZE*2)+1;
+  dim3 dimgrid(numblocks,1,1);
+  dim3 dimblock(BLOCK_SIZE,1,1);
 
   //@@ Modify this to complete the functionality of the scan
   //@@ on the deivce
+  float* tempsums;
+  wbCheck(cudaMalloc((void **)&tempsums,sizeof(float)*numblocks));
+  scan_tree<<<dimgrid,dimblock>>>(deviceInput,deviceOutput,numElements);
+  scan_getsum<<<dimgrid,dimblock>>>(deviceOutput,tempsums,numblocks);
+  scan_tree<<<1,numblocks>>>(tempsums,tempsums,numblocks);
+  scan_add<<<dimgrid,dimblock>>>(deviceOutput,tempsums,numElements);
 
   cudaDeviceSynchronize();
 
@@ -67,6 +121,9 @@ int main(int argc, char **argv) {
 
 
   //@@  Free GPU Memory
+  cudaFree(tempsums);
+  cudaFree(deviceInput);
+  cudaFree(deviceOutput);
 
 
   wbSolution(args, hostOutput, numElements);
