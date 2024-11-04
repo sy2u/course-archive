e79b531f8a043303fbab306ec32083f88acd13f9
Siying Yu <siyingy3@illinois.edu>
2024-11-04 16:26:09 -0600

    finish lab7 with gpu scan

--- lab7/lab7.cu
+++ lab7/lab7.cu
@@ -4,6 +4,7 @@
 
 #define TILE_WIDTH 16
 #define HISTOGRAM_LENGTH 256
+#define SCAN_BLOCK_WIDTH HISTOGRAM_LENGTH/2
 
   //@@ insert code here
   //---------------------------------------------------------------------------------
@@ -31,13 +32,13 @@
     }
   }
 
-  __global__ void get_hist( unsigned char* grayImage, unsigned int* histogram, int width, int height ){
+  __global__ void get_hist( unsigned char* grayImage, float* histogram, int width, int height ){
     int col = threadIdx.x + blockIdx.x * blockDim.x;
     int row = threadIdx.y + blockIdx.y * blockDim.y;
     int image_idx = row * width + col;
     int hist_idx = threadIdx.x + threadIdx.y * blockDim.x;
 
-    __shared__ unsigned int private_hist[HISTOGRAM_LENGTH];
+    __shared__ float private_hist[HISTOGRAM_LENGTH];
 
     if( hist_idx < HISTOGRAM_LENGTH ) private_hist[hist_idx] = 0;
     __syncthreads();
@@ -71,54 +72,54 @@
   //---------------------------------------------------------------------------------
   // Imported from Lab6:
   //---------------------------------------------------------------------------------
-  // __global__ void scan_tree(unsigned int *input, unsigned int *output, int len, int image_size) {
-  //   __shared__ float T[2*TILE_WIDTH];
-  //   // each thread load 2 inputs
-  //   int i = 2*(blockIdx.x*blockDim.x+threadIdx.x);
-  //   int j = i+1;
-  //   if( i < len ) T[2*threadIdx.x] = input[i] / image_size;
-  //   if( j < len ) T[2*threadIdx.x+1] = input[j] / image_size;
-  //   // first pass
-  //   int stride = 1;
-  //   while( stride < 2*TILE_WIDTH ){
-  //     __syncthreads();
-  //     int idx = (threadIdx.x+1)*stride*2 - 1;
-  //     if(idx < 2*TILE_WIDTH && (idx-stride) >= 0)
-  //     T[idx] += T[idx-stride];
-  //     stride = stride*2;
-  //   }
-  //   // second pass: post scan
-  //   stride = TILE_WIDTH/2;
-  //   while(stride > 0) {
-  //     __syncthreads();
-  //     int idx = (threadIdx.x+1)*stride*2 - 1;
-  //     if ((idx+stride) < 2*TILE_WIDTH)
-  //     T[idx+stride] += T[idx];
-  //     stride = stride / 2;
-  //   }
-  //   // copy back
-  //   __syncthreads();
-  //   if( i < len ) output[i] = T[2*threadIdx.x];
-  //   if( j < len ) output[j] = T[2*threadIdx.x+1];
-  // }
+  __global__ void scan_tree(float *input, float *output, int len, int image_size) {
+    __shared__ float T[2*SCAN_BLOCK_WIDTH];
+    // each thread load 2 inputs
+    int i = 2*(blockIdx.x*blockDim.x+threadIdx.x);
+    int j = i+1;
+    if( i < len ) T[2*threadIdx.x] = input[i] / image_size;
+    if( j < len ) T[2*threadIdx.x+1] = input[j] / image_size;
+    // first pass
+    int stride = 1;
+    while( stride < 2*SCAN_BLOCK_WIDTH ){
+      __syncthreads();
+      int idx = (threadIdx.x+1)*stride*2 - 1;
+      if(idx < 2*SCAN_BLOCK_WIDTH && (idx-stride) >= 0)
+      T[idx] += T[idx-stride];
+      stride = stride*2;
+    }
+    // second pass: post scan
+    stride = SCAN_BLOCK_WIDTH/2;
+    while(stride > 0) {
+      __syncthreads();
+      int idx = (threadIdx.x+1)*stride*2 - 1;
+      if ((idx+stride) < 2*SCAN_BLOCK_WIDTH)
+      T[idx+stride] += T[idx];
+      stride = stride / 2;
+    }
+    // copy back
+    __syncthreads();
+    if( i < len ) output[i] = T[2*threadIdx.x];
+    if( j < len ) output[j] = T[2*threadIdx.x+1];
+  }
 
-  // __global__ void scan_getsum(unsigned int *input, unsigned int *sums, int numblocks) {
-  //   // 511, 1023, ..., must be odd
-  //   if( (threadIdx.x == TILE_WIDTH-1) && (blockIdx.x < numblocks) ){
-  //     sums[blockIdx.x] = input[2*(threadIdx.x+blockIdx.x*blockDim.x)+1];
-  //   }
-  // }
+  __global__ void scan_getsum(float *input, float *sums, int numblocks) {
+    // 511, 1023, ..., must be odd
+    if( (threadIdx.x == SCAN_BLOCK_WIDTH-1) && (blockIdx.x < numblocks) ){
+      sums[blockIdx.x] = input[2*(threadIdx.x+blockIdx.x*blockDim.x)+1];
+    }
+  }
 
-  // __global__ void scan_add(unsigned int *input, unsigned int *sums, int len) {
-  //   int sum;
-  //   if( blockIdx.x!=0 ){
-  //     sum = sums[blockIdx.x-1];
-  //     int i = 2*(blockIdx.x*blockDim.x+threadIdx.x);
-  //     int j = i+1;
-  //     if( i < len ) input[i] = input[i] + sum;
-  //     if( j < len ) input[j] = input[j] + sum;
-  //   }
-  // }
+  __global__ void scan_add(float *input, float *sums, int len) {
+    float sum;
+    if( blockIdx.x!=0 ){
+      sum = sums[blockIdx.x-1];
+      int i = 2*(blockIdx.x*blockDim.x+threadIdx.x);
+      int j = i+1;
+      if( i < len ) input[i] = input[i] + sum;
+      if( j < len ) input[j] = input[j] + sum;
+    }
+  }
 
   //---------------------------------------------------------------------------------
   // CPU Code:
@@ -163,7 +164,7 @@ int main(int argc, char **argv) {
   float* deviceInput_float;
   unsigned char* deviceInput_unsigned;
   unsigned char* deviceInput_gray;
-  unsigned int* device_histogram;
+  float* device_hist;
   float* device_cdf;
   float* deviceOutput;
 
@@ -173,7 +174,7 @@ int main(int argc, char **argv) {
   cudaMalloc((void**)&deviceInput_float, sizeof(float)*all_size);
   cudaMalloc((void**)&deviceInput_unsigned, sizeof(unsigned char)*all_size);
   cudaMalloc((void**)&deviceInput_gray, sizeof(unsigned char)*all_size);
-  cudaMalloc((void**)&device_histogram, sizeof(unsigned int)*HISTOGRAM_LENGTH);
+  cudaMalloc((void**)&device_hist, sizeof(float)*HISTOGRAM_LENGTH);
   cudaMalloc((void**)&device_cdf, sizeof(float)*HISTOGRAM_LENGTH);
   cudaMalloc((void**)&deviceOutput, sizeof(float)*all_size);
 
@@ -195,26 +196,19 @@ int main(int argc, char **argv) {
   // get histogram
   dim3 BlockDim_Hist(TILE_WIDTH, TILE_WIDTH, 1);
   dim3 GridDim_Hist(ceil(imageWidth/(1.0*TILE_WIDTH)), ceil(imageHeight/(1.0*TILE_WIDTH)), 1);
-  get_hist<<<GridDim_Hist,BlockDim_Hist>>>(deviceInput_gray, device_histogram, imageWidth, imageHeight);
+  get_hist<<<GridDim_Hist,BlockDim_Hist>>>(deviceInput_gray, device_hist, imageWidth, imageHeight);
 
   // compute CDF
-  unsigned int cpu_hist[HISTOGRAM_LENGTH];
-  float cpu_cdf[HISTOGRAM_LENGTH];
-  cudaMemcpy(cpu_hist,device_histogram,sizeof(unsigned int)*HISTOGRAM_LENGTH,cudaMemcpyDeviceToHost);
-  cpu_cdf[0] = (float)cpu_hist[0]/flat_size;
-  for( int i = 1; i < HISTOGRAM_LENGTH; i++ ){
-    cpu_cdf[i] = cpu_cdf[i-1] + (float)cpu_hist[i]/flat_size;
-  }
-  cudaMemcpy(device_cdf,cpu_cdf,sizeof(float)*HISTOGRAM_LENGTH,cudaMemcpyHostToDevice);
-  // int numblocks = (HISTOGRAM_LENGTH-1)/(TILE_WIDTH*2)+1;
-  // unsigned int* tempsums;
-  // cudaMalloc((void **)&tempsums,sizeof(float)*numblocks);
-  // dim3 dimgrid(numblocks,1,1);
-  // dim3 dimblock(TILE_WIDTH,1,1);
-  // scan_tree<<<dimgrid,dimblock>>>(device_histogram,device_cdf,HISTOGRAM_LENGTH,flat_size);
-  // scan_getsum<<<dimgrid,dimblock>>>(device_cdf,tempsums,numblocks);
-  // scan_tree<<<1,numblocks>>>(tempsums,tempsums,numblocks,flat_size);
-  // scan_add<<<dimgrid,dimblock>>>(device_cdf,tempsums,HISTOGRAM_LENGTH);
+  int numblocks = (HISTOGRAM_LENGTH-1)/(TILE_WIDTH*2)+1;
+  float* tempsums;
+  cudaMalloc((void **)&tempsums,sizeof(float)*numblocks);
+  dim3 dimgrid(numblocks,1,1);
+  dim3 dimblock(SCAN_BLOCK_WIDTH,1,1);
+  scan_tree<<<dimgrid,dimblock>>>(device_hist,device_cdf,HISTOGRAM_LENGTH,flat_size);
+  scan_getsum<<<dimgrid,dimblock>>>(device_cdf,tempsums,numblocks);
+  scan_tree<<<1,numblocks>>>(tempsums,tempsums,numblocks,flat_size);
+  scan_add<<<dimgrid,dimblock>>>(device_cdf,tempsums,HISTOGRAM_LENGTH);
+  cudaFree(tempsums);
 
   // Apply Equalization
   dim3 BlockDim_Equal(TILE_WIDTH, TILE_WIDTH, imageChannels);
@@ -235,7 +229,7 @@ int main(int argc, char **argv) {
   cudaFree(deviceInput_float);
   cudaFree(deviceInput_unsigned);
   cudaFree(deviceInput_gray);
-  cudaFree(device_histogram);
+  cudaFree(device_hist);
   cudaFree(device_cdf);
   cudaFree(deviceOutput);
 
